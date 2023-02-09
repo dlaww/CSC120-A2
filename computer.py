dc93a88bdb2557d83f9aa6fe1f650cae243b48e8
Dakota Law <dlaw14@smith.edu>
2023-02-08 22:02:49 -0500

    commit 	commit:   computer.py 	commit:   oo_resale_shop.py commit

--- computer.py
+++ computer.py
@@ -1,10 +1,38 @@
 class Computer:
 
     # What attributes will it need?
-
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self, description, processor_type: str, hard_drive_capactiy: int,
+                memory: int, operating_system: str, year_made: int, 
+                price: int):
+                self.description = description
+                self.processor_type = processor_type,
+                self.hard_drive_capacity = hard_drive_capactiy,
+                self.memory = memory,
+                self.operating_system = operating_system,
+                self.year_made = year_made,
+                self.price = price
+        
     # What methods will you need?
+    #gathering all computer details
+    def computerDetails(self):
+        print(self.description)
+        print(self.processor_type)
+        print(self.hard_drive_capacity)
+        print(self.memory)
+        print(self.operating_system)
+        print(self.year_made)
+        print(self.price)
+
+def main():
+    c = Computer("MacPro", "Sierra", "750", "500", "Jaguar", "2019", "400")
+    c.computerDetails()
+main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,51 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
-
+    inventory: list
+    bank : int
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+        self.inventory = []
+        self.bank = 50000
 
     # What methods will you need?
+    # buying a computer + adding it to inventory
+    def buy(self, c: Computer):
+        self.inventory.append(c)
+        print("added", c)
+        print(self.inventory)
+    
+    # selling a computer + removing it from inventory
+    def sell(self, c: Computer):
+        if c in self.inventory:
+            self.inventory.remove(c)
+            print("removed", c)
+            print(self.inventory)
+        else:
+            print("Computer not in inventory")
+            
+    # refurbish + increasing price
+    def refurbished(self, c: Computer):
+        if c.year_made < 2000:
+            c.price = 50         # old computer, salvage parts?
+        elif c.year_made < 2012:
+            c.price = 250       # 10+ yr old computer, in workable condition
+        elif c.year_made < 2020:
+            c.price = 750       # 3+ yr old computer
+        else:
+            c.price = 1000      # like new computer
+
+def main():
+    # buy the computer
+    ResaleShop.buy.c: Computer
+    print("Computer purchased")
+
+    # print the manifest
+
+    # sell the computer
+    #print the inventory manifest
+main()
